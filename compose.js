592881ad129d7a0d7c998ea6a734bd6c1c08e556
lc <1033581609@qq.com>
2020-10-05 16:36:43 +0800

    容错处理，当没有传入函数时或者只传入了一个函数

---
+++ compose.js
@@ -0,0 +1,14 @@
+function compose(funcs) {
+    return function (...args) {
+        if (funcs.length === 0) return args;
+        if (funcs.length === 1) return funcs[0](...args);
+    };
+}
+
+const fn1 = (x, y) => x + y + 10;
+const fn2 = x => x - 10;
+const fn3 = x => x * 10;
+const fn4 = x => x / 10;
+
+let res = compose(fn1, fn3, fn2, fn4)(20, 30);
+console.log(res);
